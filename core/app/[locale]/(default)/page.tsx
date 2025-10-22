98adb411d1b1b69893c97224af26cfba39d8b3e4
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-10-22 16:58:48 -0500

    Introduce Streamable.from to move dynamic operations further down component tree

--- core/app/[locale]/(default)/page.tsx
+++ core/app/[locale]/(default)/page.tsx
@@ -4,8 +4,8 @@ import { Page as MakeswiftPage } from '~/lib/makeswift';
 // import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-2/server';
 // import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-3/server';
 // import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-4/server';
-import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-5/server';
-// import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-6/server';
+// import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-5/server';
+import { TopCategoriesExample } from '~/components/custom/top-categories/scenario-6/server';
 
 interface Params {
   locale: string;
--- core/components/custom/top-categories/scenario-6/server.tsx
+++ core/components/custom/top-categories/scenario-6/server.tsx
@@ -4,17 +4,12 @@ import { TopCategories } from './top-categories';
 import { Streamable } from '@/vibes/soul/lib/streamable';
 
 export async function TopCategoriesExample() {
-  /**
-   * Make use of Streamable.from to get a "lazy" promise that isn't executed until it's used.
-   *  - Store the streamable result in `streamableCategories`.
-   */
-  const customerAccessToken = await getSessionCustomerAccessToken();
-  const categories = getTopCategories(customerAccessToken);
+  const streamableCategories = Streamable.from(async () => {
+    const customerAccessToken = await getSessionCustomerAccessToken();
+    return await getTopCategories(customerAccessToken);
+  });
 
-  /**
-   * Pass `streamableCategories` to the `TopCategories` component.
-   */
   return <>
-    <TopCategories categories={categories} />
+    <TopCategories categories={streamableCategories} />
   </>;
 }
