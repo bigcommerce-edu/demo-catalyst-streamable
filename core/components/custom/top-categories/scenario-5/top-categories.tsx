45dbdc92494e60be8f84a277040866f1367f6e58
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-10-23 14:59:15 -0500

    Re-factor component implementation to use <Stream>

--- core/components/custom/top-categories/scenario-5/top-categories.tsx
+++ core/components/custom/top-categories/scenario-5/top-categories.tsx
@@ -3,7 +3,6 @@ import { TopCategoriesContainer } from '../top-categories-container';
 import { SubcategoryList } from '../subcategory-list';
 import { CardSkeleton } from '~/vibes/soul/primitives/card';
 import { Stream, Streamable } from '@/vibes/soul/lib/streamable';
-import { Suspense } from 'react';
 
 interface Category {
   entityId: number;
@@ -17,59 +16,34 @@ interface Category {
 }
 
 export async function TopCategories({
-  /**
-   * Alias `categories` as `streamableCategories`, since `categories` will be used in the callback.
-   */ 
-  categories,
+  categories: streamableCategories,
 }: {
-  /**
-   * Make use of the type `Stremable` instead of explicitly expecting a promise.
-   * A `Streamable` is a value that can be either a value or a promise.
-   */
-  categories: Promise<Category[]>;
+  categories: Streamable<Category[]>;
 }) {
-  /**
-   * Use `<Stream>` as an abstraction layer to handle Suspense.
-   *  - Eliminates the need for a separate component to receive the promise
-   *  - Accommodates both synchronous and asynchronous data sources
-   *  - Accommodates resolving a promise in a server-side or client-side context
-   */
   return <>
-    <Suspense fallback={<TopCategoriesSkeleton />}>
-      <TopCategoriesInner categories={categories} />
-    </Suspense>
-  </>;
-}
-
-/**
- * `TopCategoriesInner` is no longer needed in order to use Suspense. 
- * Move the main implementation back to the main component.
- */
-async function TopCategoriesInner({ 
-  categories: categoriesPromise 
-}: {
-  categories: Promise<Category[]>;
-}) {
-  const categories = await categoriesPromise;
-
-  return <>
-    <TopCategoriesContainer title="Top Categories">
-      {categories.map((category) => (
-        <div key={category.entityId}>
-          <Card
-            title={category.name}
-            image={category.image?.url ? {
-              src: category.image?.url ?? '',
-              alt: category.image?.altText ?? '',
-            } : undefined}
-            href={category.path}
-          />
-          {category.children && (
-            <SubcategoryList categories={category.children} />
-          )}
-        </div>
-      ))}
-    </TopCategoriesContainer>
+    <Stream value={streamableCategories} fallback={<TopCategoriesSkeleton />}>
+      {(categories) => (
+        
+        <TopCategoriesContainer title="Top Categories">
+          {categories.map((category) => (
+            <div key={category.entityId}>
+              <Card
+                title={category.name}
+                image={category.image?.url ? {
+                  src: category.image?.url ?? '',
+                  alt: category.image?.altText ?? '',
+                } : undefined}
+                href={category.path}
+              />
+              {category.children && (
+                <SubcategoryList categories={category.children} />
+              )}
+            </div>
+          ))}
+        </TopCategoriesContainer>
+        
+      )}
+    </Stream>
   </>;
 }
 
